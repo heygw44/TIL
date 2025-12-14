feb1643da41fae693a51267e8e08b5d35c300f08
cayman031 <cayman031@gmail.com>
2025-12-14 14:24:11 +0900

    feat: 백준 심화

---
+++ problemsolve/BOJ/심화/팰린드롬인지_확인하기.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < s.length() / 2; i++) {
+        if (s[i] != s[s.length() - i - 1]) {
+            cout << 0;
+            return 0;
+        }
+    }
+
+    cout << 1;
+    return 0;
+}
