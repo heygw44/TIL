79b0918a869e37b9c2b46ee5eae89632902e4428
cayman031 <cayman031@gmail.com>
2025-12-24 12:08:59 +0900

    feat: 백준

---
+++ problemsolve/BOJ/리스트/요세푸스_문제.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n, k;
+
+    cin >> n >> k;
+    queue<int> q;
+
+    cout << '<';
+
+    for (int i = 1; i <= n; i++) {
+        q.push(i);
+    }
+
+    while (!q.empty())
+    {
+        for (int i = 0; i < k-1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+        cout << q.front();
+        q.pop();
+
+        if (q.empty()) cout << '>';
+        else {
+            cout << ", ";
+        }
+    }
+    
+
+
+    return 0;
+}
