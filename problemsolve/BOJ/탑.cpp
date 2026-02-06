e51aee6a4e222e51bdf0ebd5808045ece3ade5d1
heygw44 <heygw44@gmail.com>
2026-02-06 10:23:14 +0900

    feat: 백준 스택

--- problemsolve/BOJ/탑.cpp
+++ problemsolve/BOJ/탑.cpp
@@ -6,22 +6,20 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
+    stack<pair<int,int>> tower; // 높이, 위치
     int n;
     cin >> n;
-
-    stack<pair<int, int>> tower;
 
     tower.push({100000001, 0});
 
     for (int i = 1; i <= n; i++) {
         int h;
         cin >> h;
-
-        while (tower.top().first < h ) {
+        while(tower.top().first < h) {
             tower.pop();
         }
         cout << tower.top().second << " ";
-        tower.push({h, i});
+        tower.push({h,i});
     }
 
     return 0;
